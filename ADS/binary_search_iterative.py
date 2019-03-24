d1e6d51e08c958c7eb6c6104e9f7a1931488b672
MaryBlue <mariasilvia.morlino@studio.unibo.it>
2019-03-24 01:44:19 +0100

    added iterative binary search algo

---
+++ ADS/binary_search_iterative.py
@@ -0,0 +1,31 @@
+def binary_search(A, v):
+    # initialize indexes
+    p = 0
+    r = len(A)
+
+    while len(A[p:r]) > 1:
+        q = (p+r)//2  # find midpoint
+
+        # update right limit of search (r) if midpoint is greater than query
+        if A[q] > v:
+            r = q
+        # update left limit of search (p) if midpoint is lesser than query
+        elif A[q] < v:
+            p = q
+        # return current midpoint index if it's equal to query
+        elif A[q] == v:
+            return q
+
+    # if we are out of the cycle, we have an array of length one.
+    # either it's the element we're looking for, or it's not present in the array
+    if A[p:r] == v:
+        return p
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    dummy = [1, 2, 3, 4, 5, 6, 8]
+    query = 7
+    res = binary_search(dummy, query)
+    print(res)
