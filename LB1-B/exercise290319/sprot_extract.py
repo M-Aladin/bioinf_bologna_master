e756732920562be4c2ed98a413672a9a0c2f2c3d
MaryBlue <mariasilvia.morlino@studio.unibo.it>
2019-04-01 01:49:38 +0200

    tested fasta extraction from uniprot_sprot.fasta

--- LB1-B/exercise290319/sprot_extract.py
+++ LB1-B/exercise290319/sprot_extract.py
@@ -1,59 +1,23 @@
-"""
-In principle we know how a FASTA format works.
-The script should take as input a list of IDs, traverse the sprot file, when a header is found AND it contains
-an ID that you are looking for, you start printing until you find another header.
-"""
+from multifasta_mapper import fastamap
+from multifasta_mapper import fastafind
+import os
 
+# create dictionary "database" from uniprot_sprot.fasta
+dbpath = "/home/maryblue/bioinfogit/LB1-B/exercise290319/uniprot_sprot.fasta"
+database = open(dbpath)
+size = os.path.getsize(dbpath)
+d = fastamap(database, size)
+database.close()
 
-def fasta_mapper(multifasta, filesize):
-    """
-    Takes a file handler of a multifasta and returns a dictionary where keys are AC numbers and values are the
-    whole sequences in fasta format
-    """
-    index = dict()
-    firstheader = multifasta.readline()
-    key = firstheader.split("|")[1]
-    value = firstheader
-    while multifasta.tell() < filesize:  # repeat cycle until end of the file is reached
+# extract desired AC from the dictionary
+inputpath = "/home/maryblue/bioinfogit/LB1-B/exercise290319/kunitz_hsa_AC"
+outputpath = inputpath + ".fasta"
+output = open(outputpath, 'w')
+input = open(inputpath)
 
-        # read a new line
-        a = multifasta.readline()
+for line in input:
+    key = line.rstrip()
+    output.write(d[key])
 
-        if a.startswith(">sp"):  # a new header is found
-            index[key] = value  # save current variables in dictionary
-
-            # overwrite key and value with new header data
-            key = a.split("|")[1]
-            value = a
-
-        else:  # the line is part of the previous entry
-            value += a  # sum the sequence to the previous value of a (i.e. concatenate string)
-
-    index[key] = value  # save the last sequence
-
-    return index
-    # value = "is for the way you look at me"
-    # key = "l"
-    # index[key] = value
-
-
-def dprint(d):
-    """
-    print dictionary in a decent way
-    """
-    print("{")
-    for i in d.keys():
-        print(i + ":")
-        print(d[i])
-    print("}")
-
-
-if __name__ == "__main__":
-    import os
-    fastapath = "/home/maryblue/bioinfogit/LB1-B/exercise290319/somesequences.fasta"
-    f = open(fastapath)
-    size = os.path.getsize(fastapath)
-    indexed = fasta_mapper(f, size)
-    f.close()
-    dprint(indexed)
-    print(indexed["Q67475"])
+input.close()
+output.close()
